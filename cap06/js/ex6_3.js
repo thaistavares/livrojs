92f03e220104320e960ebc0f0d4e0ccacc962992
thaistavares <48595315+thaistavares@users.noreply.github.com>
2022-09-21 19:22:40 -0300

    feat: cap06 em andamento

--- cap06/js/ex6_3.js
+++ cap06/js/ex6_3.js
@@ -14,7 +14,7 @@ frm.addEventListener("submit", (e) => {
     carros.push({modelo, preco});    // adiciona os dados ao vetor de objetos
     frm.inModelo.value = "";         // limpa os campos do form
     frm.inPreco.value = "";
-    frm.inModelo.focus();                // posiciona o cursor em inModelos 
+    frm.inModelo.focus();                // posiciona o cursor em inModelo
     
     // dispara um evento de click em btListar (equivale a um click no botão da página)
     frm.btListar.dispatchEvent(new Event("click"));
@@ -31,7 +31,7 @@ frm.btListar.addEventListener("click", () => {    // "escuta" clique em btListar
     // o método reduce() concatena uma string, obtendo modelo e preço de cada veículo
     const lista = carros.reduce((acumulador, carro) =>
         acumulador + carro.modelo + "- R$: " + carro.preco.toFixed(2) + "\n", "");
-    resp.innerText - `Lista dos Carros Cadastrados\n${"-".repeat(40)}\n${lista}`
+    resp.innerText = `Lista dos Carros Cadastrados\n${"-".repeat(40)}\n${lista}`;
 
 });
 
@@ -45,6 +45,7 @@ frm.btFiltrar.addEventListener("click", () => {    // "escuta" clique em btFiltr
     const carrosFilter = carros.filter(carro => carro.preco <= maximo);
     if (carrosFilter.length == 0) {    // se tamanho do vetor filtrado for zero
         alert("Não há carros com preço inferior ou igual ao solicitado");
+        return;
     }
     
     let lista = "";
@@ -66,12 +67,12 @@ frm.btSimular.addEventListener("click", () => {    // "escuta" clique em btSimul
 
     const carrosDesc = carros.map(aux => ({
         modelo: aux.modelo,
-        preco: aux.preco - (aux.preco * desconto / 100);
+        preco: aux.preco - (aux.preco * desconto / 100)
     }))
 
     let lista ="";
    
-    for (const carro of carros) {    // percorre cada elemento do array 
+    for (const carro of carrosDesc) {    // percorre cada elemento do array 
         lista += `${carro.modelo} - R$: ${carro.preco.toFixed(2)}\n`;
     }
     
