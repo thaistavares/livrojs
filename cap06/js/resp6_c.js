da9b537ca4415819169572fcb18ba1d4254ed820
ThaisTavares <thais.tavares@gmail.com>
2022-09-23 23:21:03 -0300

    feat: cap06 concluido

--- cap06/js/resp6_c.js
+++ cap06/js/resp6_c.js
@@ -2,16 +2,16 @@
 const frm = document.querySelector("form");
 const resp = document.querySelector("pre");
 
-const criancas = [];    // declara vetor global
+const candidatos = [];    // declara vetor global
 
 // cria um "ouvinte" de evento, acionado quando o botão submit for clicado
 frm.addEventListener("submit", (e) => {
     e.preventDefault();           // evita o envio do form
     
     const nome = frm.inNome.value;          // obtém os dados do form
-    const idade = Number(frm.inIdade.value);
+    const acerto = Number(frm.inAcerto.value);
     
-    criancas.push({nome, idade});    // adiciona os dados ao vetor de objetos
+    candidatos.push({nome, acerto});    // adiciona os dados ao vetor de objetos
     frm.reset();         // limpa os campos do form
     frm.inNome.focus();                // posiciona o cursor em inModelos 
     
@@ -22,49 +22,46 @@ frm.addEventListener("submit", (e) => {
 
 // cria um "ouvinte" de evento, acionado quando o botão Listar for clicado
 frm.btListar.addEventListener("click", () => {    // "escuta" clique em btListar
-    if (criancas.length == 0) {                     // se o tamanho do vetor for igual a zero
-        alert("Não há crianças na lista");
+    if (candidatos.length == 0) {                     // se o tamanho do vetor for igual a zero
+        alert("Não há candidatos na lista");
         return;
     }
 
-    let lista = "";    // para concatenar lista de crianças
-    for (const crianca of criancas) {
-        const {nome, idade} = crianca;    // desestrutura o objeto
-        lista += nome + " - " + idade + " anos\n";
+    let lista = "";    // para concatenar lista de candidatos
+    for (const candidato of candidatos) {
+        const {nome, acerto} = candidato;    // desestrutura o objeto
+        lista += nome + " - " + acerto + " acertos\n";
     }
     
     resp.innerText = lista;    // exibe a lista
 
 });
 
-// cria um "ouvinte" de evento, acionado quando o botão Resumir for clicado
-frm.btResumir.addEventListener("click", () => {    // "escuta" clique em btResumir
-    if (criancas.length == 0) {
-        alert("Não há crianças na lista")   
+// cria um "ouvinte" de evento, acionado quando o botão Aprovados for clicado
+frm.btAprovados.addEventListener("click", () => {    // "escuta" clique em btAprovados
+    if (candidatos.length == 0) {
+        alert("Não há candidatos na lista")   
         return;                       
     }
 
-    const copia = [...criancas];    // cria a cópia do vetor criança
-    copia.sort((a, b) => a.idade - b.idade);    // ordena pela idade
-    let resumo = "";    // para concatenar a saída
-    let aux = copia[0].idade;    // menor idade do vetor ordenado
-    let nomes = [];    // para inserir nomes de cada idade
-    for (const crianca of copia) {
-        const {nome, idade} = crianca;
-        if (idade == aux) {    // se é da mesma idade auxiliar...
-            nomes.push(nome);  // adiciona ao vetor nomes
-        } else {    // senão, monta o resumo para cada idade
-            resumo += aux + " ano(s): " + nomes.length + " criança(s) - ";
-            resumo += ((nomes.length / copia.length) * 100).toFixed(2) + "%\n";
-            resumo += "(" + nomes.join(", ") + ")\n\n";
-            aux = idade;    // obtém a nova idade na ordem
-            nomes = [];     // limpa o vetor dos nomes
-            nomes.push(nome);    // adiciona o primeiro da nova idade
-        }
+    const valorAcerto = Number(prompt("Número de Acertos para Aprovação? "));
+    if ((valorAcerto == 0) || (isNaN(valorAcerto))) {    // se não informou ou valor inválido
+        return;                                // ... retorna
     }
-    // adiciona a última criança
-    resumo += aux + " ano(s): " + nomes.length + " criança(s) - ";
-    resumo += ((nomes.length / copia.length) * 100).toFixed(2) + "%\n";
-    resumo += "(" + nomes.join(", ") + ")\n\n";
-    resp.innerText = resumo;    // exibe a resposta
+
+    // cria um novo vetor com os objetos que atendem à condição de filtro
+    const candidatosFilter = candidatos.filter(candidato => candidato.acerto >= valorAcerto);
+    if (candidatosFilter.length == 0) {    // se tamanho do vetor filtrado for zero
+        alert("Não há candidatos aprovados");
+        return;
+    }
+    
+    let lista = "";
+    
+    for (const candidato of candidatosFilter) {    // percorre cada elemento do array
+        lista += `${candidato.nome} - ${candidato.acerto} acertos\n`;
+    }
+
+    resp.innerText = lista;
+
 });
