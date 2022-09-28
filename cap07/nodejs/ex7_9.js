c1f9b3253535608fedde0769bec3473ffd0110c7
thaistavares <thais.tavares@gmail.com>
2022-09-28 13:55:17 -0300

    feat: cap07 em andamento

---
+++ cap07/nodejs/ex7_9.js
@@ -0,0 +1,26 @@
+const prompt = require("prompt-sync")();    // pacote para entrada de dados
+
+const formula = prompt("Fórmula: ");    // lê a fórmula
+let abre = 0;
+let fecha = 0;
+
+for (const simbolo of formula) {    // percorre os caracteres da fórmula
+    if (simbolo == "(") {
+        abre++;
+    } else if (simbolo == ")") {
+        fecha++;
+    }
+    
+    // se, em algum momento, o número de fecha for maior que de abre...
+    if (fecha > abre) {
+        break;    // ...sai da repetição
+    }
+}
+
+if (abre == fecha) {
+    console.log("Ok! Fórmula correta (em relação aos parênteses)");
+} else {
+    console.log("Erro... Fórmula incorreta");
+}
+
+ 
