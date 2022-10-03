ec343524674acc877f790edd4b06785ffc99d240
thaistavares <thais.tavares@gmail.com>
2022-10-03 13:54:07 -0300

    feat: cap08 em andamento

--- cap08/js/ex8_2.js
+++ cap08/js/ex8_2.js
@@ -1,39 +1,43 @@
 // cria referência ao form e elementos de resposta do programa
 const frm = document.querySelector("form");
-const resp = document.querySelector("h3");
-
-const TAXA_MULTA = 2 / 100;       // multa por atraso
-const TAXA_JUROS = 0.33 / 100;    // juros por dia de atraso
+const resp1 = document.querySelector("#outResp1");
+const resp2 = document.querySelector("#outResp2");
+const resp3 = document.querySelector("#outResp3");
 
 // cria um "ouvinte" de evento, acionado quando o botão submit for clicado
 frm.addEventListener("submit", (e) => {
     e.preventDefault();           // evita o envio do form
     
-    const dataVenc = frm.inDataVenc.value;    // obtém a data de vencimento informada pelo usuário
-    const valor = Number(frm.inValor.value);  // obtém o valor da conta informado pelo usuário  
-    const hoje = new Date();      // cria variáveis (instancia objetos)
-    const vencto = new Date();    // do tipo Date() 
+    const modelo = frm.inModelo.value;    // obtém o modelo do veículo informado pelo usuário
+    const ano = Number(frm.inAno.value);  // obtém o ano do veículo informado pelo usuário
+    const preco = Number(frm.inPreco.value);  // obtém o preço do veículo informado pelo usuário
+    
+    const classificacao = classificarVeiculo(ano);    // chama funções e atribui
+    const entrada = calcularEntrada(preco, classificacao);    // ... retorno às variáveis
+    const parcela = (preco - entrada) / 10;    // usa retorno da função para cálculo
 
-    const partes = dataVenc.split("-");    // data vem no formato aaaa-mm-dd
-    vencto.setDate(Number(partes[2]));
-    vencto.setMonth(Number(partes[1]) - 1);
-    vencto.setFullYear(Number(partes[0]));
-
-    const atraso = hoje - vencto;    // calcula a diferença de dias entre datas (em ms)
-    let multa = 0;    // inicializa multa e juros com zero
-    let juros = 0;
-
-    if (atraso > 0) {    // se a conta estiver em atraso...
-        // converte ms do atraso em dias (1 dia = 24h x 60min x 60s x 1000ms: 86400000)
-        const dias  = atraso / 86400000;
-        multa = valor * TAXA_MULTA;    // calcula a multa e os juros
-        juros = valor * TAXA_JUROS * dias;
-    }
-
-    const total = valor + multa + juros;    // calcula o total
-
-    frm.outMulta.value = multa.toFixed(2);    // exibe os valores com dois decimais
-    frm.outJuros.value = juros.toFixed(2);
-    frm.outTotal.value = total.toFixed(2);
+    resp1.innerText = modelo + " - " + classificacao;    // exibe as respostas
+    resp2.innerText = `Entrada R$${entrada.toFixed(2)}`;
+    resp3.innerText = `+10x de R$${parcela.toFixed(2)}`;
 
 });
+
+// função recebe o ano do veículo como parâmetro
+const classificarVeiculo = (ano) => {
+    const anoAtual = new Date().getFullYear();    // obtém o ano atual
+    let classif
+    if (ano == anoAtual) {    // condições para definir classificação do veículo
+        classif = "Novo";
+    } else if (ano == anoAtual - 1 || ano == anoAtual - 2) {
+        classif = "Seminovo";
+    } else {
+        classif = "Usado";
+    }
+
+    return classif;    // retorna a classificação
+
+}
+
+// função recebe valor e status do veículo como parâmetro
+const calcularEntrada = (valor, status) =>
+    status == "Novo" ? valor * 0.5 : valor * 0.3;
---
+++ cap08/js/ex8_4.js
@@ -0,0 +1,72 @@
+// cria referência ao form e elementos de resposta do programa
+const frm = document.querySelector("form");
+const resp = document.querySelector("pre");
+
+const itens = [];    // vetor global para armazenar os itens do pedido
+
+frm.rbPizza.addEventListener("click", () => {    // quando radio button é clicado
+    frm.inBebida.className = "oculta";    // oculta select das bebidas - está no estilos.css
+    frm.inPizza.className = "exibe";    // exibe select das pizzas - está no estilos.css
+});
+
+frm.rbBebida.addEventListener("click", () => {    // quando radio button é clicado
+    frm.inPizza.className = "oculta";    // oculta select das pizzas - está no estilos.css
+    frm.inBebida.className = "exibe";    // exibe select das bebidas - está no estilos.css
+});
+
+frm.inDetalhes.addEventListener("focus",() => {    // quando o campo recebe o foco
+    if (frm.rbPizza.checked) {    // se radiobutton rbPizza estiver marcado
+        const pizza = frm.inPizza.value;    // obtém value do item selecionado
+        // uso do operador ternário para indicar o número de sabores
+        const num = pizza == "media"? 2 : pizza == "grande"? 3 : 4;
+        // atributo placeholder exibe uma dica de preenchimento do campo
+        frm.inDetalhes.placeholder = `Até ${num} sabores`;
+    }
+});
+
+frm.inDetalhes.addEventListener("blur", () => {    // quando o campo perde o foco
+    frm.inDetalhes.placeholder = "";    // limpa a dica de preenchimento
+})
+
+// cria um "ouvinte" de evento, acionado quando o botão submit for clicado
+frm.addEventListener("submit", (e) => {
+    e.preventDefault();           // evita o envio do form
+
+    let produto;
+
+    if (frm.rbPizza.checked) {
+        const num = frm.inPizza.selectedIndex;    // obtém o nº. do item selecionado
+        produto = frm.inPizza.options[num].text;  // texto do item selecionado
+    } else {
+        const num = frm.inBebida.selectedIndex;    // obtém o nº. do item selecionado
+        produto = frm.inBebida.options[num].text;  // texto do item selecionado
+    }
+
+    const detalhes = frm.inDetalhes.value;         // conteúdo do inDetalhes
+    itens.push(produto + " (" + detalhes + ")");    // adiciona ao vetor
+    resp.innerText = itens.join("\n");             // exibe os itens do pedido
+
+    frm.reset();    // limpa o form
+    frm.rbPizza.dispatchEvent(new Event("click"));  // dispara click em rbPizza
+
+});
+
+// função recebe o ano do veículo como parâmetro
+const classificarVeiculo = (ano) => {
+    const anoAtual = new Date().getFullYear();    // obtém o ano atual
+    let classif
+    if (ano == anoAtual) {    // condições para definir classificação do veículo
+        classif = "Novo";
+    } else if (ano == anoAtual - 1 || ano == anoAtual - 2) {
+        classif = "Seminovo";
+    } else {
+        classif = "Usado";
+    }
+
+    return classif;    // retorna a classificação
+
+}
+
+// função recebe valor e status do veículo como parâmetro
+const calcularEntrada = (valor, status) =>
+    status == "Novo" ? valor * 0.5 : valor * 0.3;
