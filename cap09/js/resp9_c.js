26cf1f886169904030320259d197f2f9f6ff6de4
thaistavares <thais.tavares@gmail.com>
2022-10-11 10:14:24 -0300

    feat: exercicio 9.c em andamento

--- cap09/js/resp9_c.js
+++ cap09/js/resp9_c.js
@@ -1,77 +1,77 @@
 // cria referência ao form e elementos de resposta do programa
 const frm = document.querySelector("form");
-const respLista = document.querySelector("pre");
+const resp1 = document.querySelector("#outResp1");
+const resp2 = document.querySelector("#outResp2");
 
 // cria um "ouvinte" de evento, acionado quando o botão submit for clicado
 frm.addEventListener("submit", (e) => {
     e.preventDefault();           // evita o envio do form
 
-    const produto = frm.inProduto.value;            // conteúdo do inProduto
+    const servico = frm.inServico.value;            // conteúdo do inServico
 
     // chama function que verifica se produto já foi adicionado
-    if (verProdutoExiste(produto)) {
-        alert("Este produto já foi adicionado. Por favor, informe outro...");
-        frm.inProduto.focus();
+    if (verServicoExiste(servico)) {
+        alert("Este servico já foi adicionado. Por favor, informe outro...");
+        frm.inServico.focus();
         return;
     }
 
-    if (localStorage.getItem("produtoNome")) {    // se houver dados em localStorage
-        // obtém o conteúdo já salvo e acrescenta ";" + dados do produto
-        const produtoNome = localStorage.getItem("produtoNome") + ";" + produto;
-        localStorage.setItem("produtoNome", produtoNome);    // salva o produto
+    if (localStorage.getItem("servicoNome")) {    // se houver dados em localStorage
+        // obtém o conteúdo já salvo e acrescenta ";" + dados do serviço
+        const servicoNome = localStorage.getItem("servicoNome") + ";" + servico;
+        localStorage.setItem("servicoNome", servicoNome);    // salva o serviço
     } else {    // senão, é a primeira vez que adiciona um produto
-        localStorage.setItem("produtoNome", produto);    // salva o produto (sem ";")
+        localStorage.setItem("servicoNome", servico);    // salva o serviço (sem ";")
     }
 
-    mostrarProdutos();      // chama function que mostra os produtos já salvos
-    frm.reset();           // limpa o form
-    frm.inProduto.focus();    // joga o foco (cursor) no campo inProduto
+    mostrarServicos();        // chama function que mostra os serviços já salvos
+    frm.reset();              // limpa o form
+    frm.inServico.focus();    // joga o foco (cursor) no campo inServico
 
 });
 
-    const verProdutoExiste = (produto) => {
-        if (localStorage.getItem("produtoNome")) {    // se existir dados em localStorage
+    const verServicoExiste = (servico) => {
+        if (localStorage.getItem("servicoNome")) {    // se existir dados em localStorage
             // obtém seu conteúdo e a string é dividida em itens de vetor a cada ";"
-            const produtos = localStorage.getItem("produtoNome").split(";");
-            // o produto deve ser convertido em string, pois o vetor contém strings
-            return produtos.includes(produto.toString());
+            const servicos = localStorage.getItem("servicoNome").split(";");
+            // o serviço deve ser convertido em string, pois o vetor contém strings
+            return servicos.includes(servico.toString());
         } else {
             return false;
         }
     }
 
-    const mostrarProdutos = () => {
-        // se não há produtos armazenados em localStorage
-        if (!localStorage.getItem("produtoNome")) {
-            // limpa o espaço de exibição dos produtos (para quando "Limpar Lista")
-            respLista.innerText = "";
+    const mostrarServicos = () => {
+        // se não há serviços armazenados em localStorage
+        if (!localStorage.getItem("servicoNome")) {
+            // limpa o espaço de exibição dos serviços
+            resp2.innerText = "";
             return;    // retorna (não executa os comandos abaixo)
         }
 
         // obtém o conteúdo da variável salva no localStorage, separando-o
         // em elementos de vetor a cada ocorrência do ";"
-        const produtos = localStorage.getItem("produtoNome").split(";");
+        const servicos = localStorage.getItem("servicoNome").split(";");
         
-        let ordem = produtos.sort();    // ordena o vetor produtos em ordem alfabética
         let linhas = "";
 
         // repetição para percorrer todos os elementos do vetor
-        for (let i = 0; i < produtos.length; i++) {
-            linhas += `${ordem[i]}\n`;
+        for (let i = 0; i < servicos.length; i++) {
+            linhas += `${servicos[i]}\n`;
         }
 
         //exibe as linhas (altera o conteúdo do elemento respLista)
-        respLista.innerText = `Produtos Adicionados\n--------------------\n${linhas}`;
+        resp1.innerText = ${linhas};
 
     };
 
-    // chama a function quando a página é carregada para mostrar os produtos salvos
-    window.addEventListener("load", mostrarProdutos);
+    // chama a function quando a página é carregada para mostrar os serviços salvos
+    window.addEventListener("load", mostrarServicos);
 
-    frm.btLimpar.addEventListener("click", () => {
+    frm.btExecutar.addEventListener("click", () => {
         
-        // solicita confirmação para excluir os produtos
-        if (confirm("Confirma exclusão de todos os produtos?")) {
+        // solicita confirmação para excluir os serviços
+        if (confirm("Confirma exclusão de todos os serviços?")) {
             localStorage.removeItem("produtoNome"); // remove as variável salva em localStorage
             mostrarProdutos(); // exibe a listagem vazia
         }
