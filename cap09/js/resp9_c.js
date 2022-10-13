11d711e5ed0a41bb65977ba7cec6a4009c90a958
thaistavares <thais.tavares@gmail.com>
2022-10-13 10:02:46 -0300

    feat: cap09 concluido

--- cap09/js/resp9_c.js
+++ cap09/js/resp9_c.js
@@ -1,7 +1,26 @@
 // cria referência ao form e elementos de resposta do programa
 const frm = document.querySelector("form");
-const resp1 = document.querySelector("#outResp1");
-const resp2 = document.querySelector("#outResp2");
+const resp1 = document.querySelector("span");
+const resp2 = document.querySelector("h4");
+
+
+const verServicoExiste = (servico) => {
+    if (localStorage.getItem("servicoNome")) {    // se existir dados em localStorage
+        // obtém seu conteúdo e a string é dividida em itens de vetor a cada ";"
+        const servicos = localStorage.getItem("servicoNome").split(";");
+        // o serviço deve ser convertido em string, pois o vetor contém strings
+        return servicos.includes(servico.toString());
+    } else {
+        return false;
+    }
+}
+
+const mostrarServicos = () => {
+
+    const servicos = localStorage.getItem("servicoNome").split(";");
+    resp1.innerText = `${servicos.length}`;
+
+}
 
 // cria um "ouvinte" de evento, acionado quando o botão submit for clicado
 frm.addEventListener("submit", (e) => {
@@ -24,56 +43,27 @@ frm.addEventListener("submit", (e) => {
         localStorage.setItem("servicoNome", servico);    // salva o serviço (sem ";")
     }
 
-    mostrarServicos();        // chama function que mostra os serviços já salvos
     frm.reset();              // limpa o form
     frm.inServico.focus();    // joga o foco (cursor) no campo inServico
+    mostrarServicos();
+
+    return servicoNome;
 
 });
-
-    const verServicoExiste = (servico) => {
-        if (localStorage.getItem("servicoNome")) {    // se existir dados em localStorage
-            // obtém seu conteúdo e a string é dividida em itens de vetor a cada ";"
-            const servicos = localStorage.getItem("servicoNome").split(";");
-            // o serviço deve ser convertido em string, pois o vetor contém strings
-            return servicos.includes(servico.toString());
-        } else {
-            return false;
-        }
-    }
-
-    const mostrarServicos = () => {
-        // se não há serviços armazenados em localStorage
-        if (!localStorage.getItem("servicoNome")) {
-            // limpa o espaço de exibição dos serviços
-            resp2.innerText = "";
-            return;    // retorna (não executa os comandos abaixo)
-        }
-
-        // obtém o conteúdo da variável salva no localStorage, separando-o
-        // em elementos de vetor a cada ocorrência do ";"
-        const servicos = localStorage.getItem("servicoNome").split(";");
-        
-        let linhas = "";
-
-        // repetição para percorrer todos os elementos do vetor
-        for (let i = 0; i < servicos.length; i++) {
-            linhas += `${servicos[i]}\n`;
-        }
-
-        //exibe as linhas (altera o conteúdo do elemento respLista)
-        resp1.innerText = ${linhas};
-
-    };
 
     // chama a function quando a página é carregada para mostrar os serviços salvos
     window.addEventListener("load", mostrarServicos);
 
     frm.btExecutar.addEventListener("click", () => {
-        
-        // solicita confirmação para excluir os serviços
-        if (confirm("Confirma exclusão de todos os serviços?")) {
-            localStorage.removeItem("produtoNome"); // remove as variável salva em localStorage
-            mostrarProdutos(); // exibe a listagem vazia
+
+        const servicos = localStorage.getItem("servicoNome").split(";");
+        localStorage.setItem("servicoNome", servicos);
+        const execucao = servicos.shift();
+        resp2.innerText = `${execucao}`;
+        localStorage.setItem("servicoNome", servicos.join(";"));    // salva a nova lista (sem o removido)
+        resp1.innerText = `${servicos.length}`;
+        if (servicos.length == 0) {
+            localStorage.removeItem("servicoNome");
         }
-    
+        
     });
