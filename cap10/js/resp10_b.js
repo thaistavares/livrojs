2011179032915ad8837b1fccb96ea3d633db34a8
thaistavares <48595315+thaistavares@users.noreply.github.com>
2022-10-17 17:16:45 -0300

    feat: exercicios do cap10 em andamento

--- cap10/js/resp10_b.js
+++ cap10/js/resp10_b.js
@@ -21,12 +21,24 @@ frm.addEventListener("submit", (e) => {
     const nome = frm.inNome.value;    // conteúdo do inNome
     const partes = nome.split(" ");
 
+    if (partes.length == 1) {
+        alert("Informe o nome completo");
+        frm.inNome.focus();
+        return;
+    }
+
+    // para retirar os h3 já existentes (se clicou uma segunda vez no botão) 
+    const listaH3 = document.querySelectorAll("h3")
+    for (let i = 0; i < listaH3.length; i++) {
+        dvQuadro.removeChild(listaH3[i]);
+    }
+
     for (let parte of partes) {
-        const h3 = document.createElement("h3");        // cria o elemento HTML h3
+        const h3 = document.createElement("h3");         // cria o elemento HTML h3
         const texto = document.createTextNode(parte);    // cria um texto
         h3.appendChild(texto);       // define que texto será filho de h3
-        h3.style.color = gerarCor();
-        dvQuadro.appendChild(h3);    // e que h3 será filho de divQuadro
+        h3.style.color = gerarCor(); // gera as cores aleatórias
+        dvQuadro.appendChild(h3);    // define que h3 será filho de divQuadro
     }
 
     frm.inNome.value = "";     // limpa o campo de edição
