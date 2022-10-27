af83a1c5c231e6e4a1ee6500d413369d5c5e00cc
thaistavares <thais.tavares@gmail.com>
2022-10-27 14:11:15 -0300

    feat: cap13 concluido e livro finalizado

---
+++ cap13/src/components/ItemLista.js
@@ -0,0 +1,24 @@
+import "./ItemLista.css";
+
+const ItemLista = ({id, titulo, autor, ano, preco, foto, excluirClick, alterarClick}) => {
+    return (
+        <tr>
+            <td>{id}</td>
+            <td>{titulo}</td>
+            <td>{autor}</td>
+            <td>{ano}</td>
+            <td class="text-end">
+                {Number(preco).toLocaleString("pt-br", { minimumFractionDigits: 2 })}
+            </td>
+            <td class="text-center">
+                <img src={foto} alt="Capa do Livro" width="75" />
+            </td>
+            <td class="text-center">
+                <i className="exclui text-danger fw-bold" title="Excluir" onClick={excluirClick}>&#10008;</i>
+                <i className="altera text-success fw-bold ms-2" title="Alterar" onClick={alterarClick}>&#36;</i>
+            </td>
+        </tr>
+    );
+};
+
+export default ItemLista;
